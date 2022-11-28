58afe952d69a7986a983ff056e9ec6c7cb7f3903
Pablo - Gmail <pablo.delahera@sdggroup.com>
2022-11-28 11:44:50 +0000

    nueva rama de trabajo

--- models/Business/dim/dim_partsupp.sql
+++
@@ -1,9 +0,0 @@
-{{ config(schema='FIN') }}
-
-select ps.ART_ID,
-ps.PROVEEDOR_ID,
-ps.CANTIDAD_DISPONIBLE,
-ps.COSTE_SUMINISTRO,
-ds.nombre_nacion,
-ds.nombre_region
-from {{ ref('stg_partsupp') }} ps left join {{ ref('dim_suplier') }} ds on ps.PROVEEDOR_ID = ds.id
--- models/transforming/tr_fct_pedidos_pais.sql
+++
@@ -1,8 +0,0 @@
-{{ config(schema='AUX', unique_key = 'nombre_nacion') }}
-
-select nombre_nacion,
-avg(NUMBER_OF_ORDERS) as Pedidos_pais,
-avg(GASTO_PEDIDOS) as gasto_medio_pais
-from {{ref('tr_fct_pre_customer')}}
-group by nombre_nacion
-order by gasto_medio_pais desc
